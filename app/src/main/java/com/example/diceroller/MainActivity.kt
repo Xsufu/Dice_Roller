45a8c14f5167166c959699ed8eda9cd8e9ce6f46
Xsufu <aluno.macarrao@gmail.com>
2022-03-05 15:51:47 +0700

    Проведены рефакторинг и комментирование кода

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,26 +5,39 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * Данная активность позволяет пользователю
+ * бросить кости и увидеть результат на экране
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Находим кнопку в активности
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        //Устанавливаем прослушку нажатия и вызываем метод броска
+        rollButton.setOnClickListener { rollDice() }
     }
 
+    /**
+     * Бросок костей и вывод результата на экран
+     */
     private fun rollDice() {
+        //Создаём кубик с шестью сторонами
         val dice = Dice(6)
+        //Бросаем кубик
         val diceRoll = dice.roll()
+        //Указываем на место вывода результата
         val resultTextView: TextView = findViewById(R.id.textView)
+        //Выводим результат на экран
         resultTextView.text = diceRoll.toString()
     }
 }
 
-//Бросок кубика
+/**
+ * Бросок кубика
+ */
 class Dice(private val numSides: Int) {
     fun roll(): Int {
         return (1..numSides).random()
